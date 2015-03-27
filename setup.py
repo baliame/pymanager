93c2b6aa785d3b80424f81247da5bde948349195
Ákos Tóth <akos.toth@cheppers.com>
2015-03-27 22:57:41 +0100

    reversioning to keep in sync with pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ description='A process manager in Python.'
 
 setup(
     name='pymanager',
-    version='0.1.2',
+    version='0.1.3',
     description=description,
     long_description=description,
     url='https://github.com/baliame/pymanager',
